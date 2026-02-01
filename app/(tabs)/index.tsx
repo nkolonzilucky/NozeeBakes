eb3bc65c1edfef96513ea751c36b8bc3b778ab8d
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2026-02-01 09:56:22 +0200

    Corrected the location display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -33,8 +33,13 @@ export default function MenuScreen() {
     <View style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
-        <Text style={styles.title}>25 min · Home</Text>
-        <Text style={styles.subtitle}>Liberty ave, 47</Text>
+        <View style={{ flexDirection: "row", gap: 12 }}>
+          <Text style={styles.title}>📍 25min·Home</Text>
+          <Text style={styles.subtitle1}>Liberty ave, 47</Text>
+        </View>
+        <Text style={styles.subtitle2}>
+          Kitchen opens 11:00 - 14:00 and 16:30 - 20:30
+        </Text>
       </View>
 
       {/* Categories */}
@@ -79,13 +84,21 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   header: {
+    gap: 2,
+    backgroundColor: "yellow",
+    alignItems: "center",
     marginBottom: 16,
   },
   title: {
     fontSize: 18,
-    fontWeight: "700",
+    fontWeight: "600",
   },
-  subtitle: {
+  subtitle1: {
+    fontWeight: "400",
+    fontSize: 18,
+    color: "black",
+  },
+  subtitle2: {
     color: "#2F6F3E",
   },
   categories: {
